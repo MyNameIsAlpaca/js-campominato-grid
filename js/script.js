5e040c4ac7f7af4d50c75724d72cc309f2bdbe7a
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-03-03 16:14:58 +0100

    feat: bonuss

--- js/script.js
+++ js/script.js
@@ -16,29 +16,90 @@ startGame.addEventListener("click", function(){
 
     difficultChoose = document.getElementById("difficult-choose").value;
 
+    if(difficultChoose == "choose") {
+        alert("Scegli una difficoltà!")
+    } else if (difficultChoose == "hard") {
+        //creo le caselle
     
-    //creo le caselle
-
-    for (let i = 1; i < 101; i++) {
+        for (let i = 1; i < 101; i++) {
+            
+            let square = document.createElement("div");
         
-        let square = document.createElement("div");
+            square.classList.add("hard-square", "square");
+
+            //aggiungo la classe al container
+
+            gridContainer.classList.add("grid-container-hard")
     
-        square.classList.add("square");
-
-        //appendo le caselle al suo contenitore 
-
-        gridContainer.append(square);
-
-        //assegno ad ogni square un numero da uno a 100
-
-        square.innerText = i;
-
-        //aggiungo background al click agli square
+            //appendo le caselle al suo contenitore 
+    
+            gridContainer.append(square);
+    
+            //assegno ad ogni square un numero da uno a 100
+    
+            square.innerText = i;
+    
+            //aggiungo background al click agli square
+            
+            square.addEventListener("click", function(){
+                square.classList.add("colorBackground");
+            })
+        }
+    }  else if (difficultChoose == "medium") {
+        //creo le caselle
+    
+        for (let i = 1; i < 82; i++) {
+            
+            let square = document.createElement("div");
         
-        square.addEventListener("click", function(){
-            square.classList.add("colorBackground");
-        })
-    }
+            square.classList.add("medium-square", "square");
+
+            //aggiungo la classe al container
+
+            gridContainer.classList.add("grid-container-medium")
+    
+            //appendo le caselle al suo contenitore 
+    
+            gridContainer.append(square);
+    
+            //assegno ad ogni square un numero da uno a 100
+    
+            square.innerText = i;
+    
+            //aggiungo background al click agli square
+            
+            square.addEventListener("click", function(){
+                square.classList.add("colorBackground");
+            })
+        }
+    } else if (difficultChoose == "easy") {
+        //creo le caselle
+    
+        for (let i = 1; i < 50; i++) {
+            
+            let square = document.createElement("div");
+        
+            square.classList.add("easy-square", "square");
+
+            //aggiungo la classe al container
+
+            gridContainer.classList.add("grid-container-easy")
+    
+            //appendo le caselle al suo contenitore 
+    
+            gridContainer.append(square);
+    
+            //assegno ad ogni square un numero da uno a 100
+    
+            square.innerText = i;
+    
+            //aggiungo background al click agli square
+            
+            square.addEventListener("click", function(){
+                square.classList.add("colorBackground");
+            })
+        }
+    } 
 
 
 
